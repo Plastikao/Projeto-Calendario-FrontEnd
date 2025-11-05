5ada95479f52da9cc0476cebab56b2b8122da5b8
AronCadeiraPng <ens-aronvargas@ugv.edu.br>
2025-11-05 16:27:59 -0300

    com o router funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css'
 import Layout from './Component/Layout'
+import Rooms from './Component/Rooms'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
-    return (
-        <>
-            <Layout />
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+                <Route element={<Layout/>} >
+
+                    <Route path="/rooms" element={<Rooms />} />
+
+                </Route>
+            </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
